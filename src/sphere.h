57f6dab963bc9dcd88d4a38ff56459dfda8d5f20
Jon O'Connell <jjoc0205@gmail.com>
2025-10-28 13:03:37 -0400

    10.3 lambertian scatter

--- src/sphere.h
+++ src/sphere.h
@@ -31,6 +31,7 @@ class sphere : public hittable {
         rec.p = r.at(rec.t);
         vec3 outward_normal = (rec.p - center) / radius;
         rec.set_face_normal(r, outward_normal);
+		rec.mat = mat;
 
         return true;
     }
@@ -38,6 +39,7 @@ class sphere : public hittable {
   private:
     point3 center;
     double radius;
+	shared_ptr<material> mat;
 };
 
 #endif
