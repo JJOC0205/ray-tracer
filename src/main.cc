214b5678e80055947abe7f4053389338b3da1bed
Jon O'Connell <jjoc0205@gmail.com>
2025-11-03 23:00:27 -0500

    starting perlin noise

--- src/main.cc
+++ src/main.cc
@@ -102,6 +102,30 @@ void checkered_spheres() {
     cam.render(world);
 }
 
+void perlin_spheres() {
+    hittable_list world;
+
+    auto pertext = make_shared<noise_texture>();
+    world.add(make_shared<sphere>(point3(0,-1000,0), 1000, make_shared<lambertian>(pertext)));
+    world.add(make_shared<sphere>(point3(0,2,0), 2, make_shared<lambertian>(pertext)));
+
+    camera cam;
+
+    cam.aspect_ratio      = 16.0 / 9.0;
+    cam.image_width       = 400;
+    cam.samples_per_pixel = 100;
+    cam.max_depth         = 50;
+
+    cam.vfov     = 20;
+    cam.lookfrom = point3(13,2,3);
+    cam.lookat   = point3(0,0,0);
+    cam.vup      = vec3(0,1,0);
+
+    cam.defocus_angle = 0;
+
+    cam.render(world);
+}
+
 void earth(){
     auto earth_texture = make_shared<image_texture>("src/images/earthmap.jpg");
     auto earth_surface = make_shared<lambertian>(earth_texture);
@@ -125,9 +149,10 @@ void earth(){
 }
 
 int main(){
-    switch (3){
+    switch (4){
         case 1: bouncing_spheres(); break;
         case 2: checkered_spheres(); break;
         case 3: earth(); break;
+        case 4:  perlin_spheres();     break;
     }
 }
